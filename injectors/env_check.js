98553f7775dfe6beca1ccf64ba2ea690b5338a91
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2020-04-22 22:16:00 -0400

    update

---
+++ injectors/env_check.js
@@ -0,0 +1,52 @@
+/**
+ * Powercord, a lightweight @discordapp client mod focused on simplicity and performance
+ * Copyright (C) 2018-2020  aetheryx & Bowser65
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { resolve } = require('path');
+const { execSync } = require('child_process');
+
+module.exports = () => {
+  // Don't clone in System32
+  if (__dirname.toLowerCase().replace(/\\/g, '/').includes('/windows/system32/')) {
+    console.error('Powercord shouldn\'t be cloned in System32, as this will generate conflicts and bloat your Windows installation. Please remove it and clone it in another place.\n' +
+      'Note: Not opening cmd as administrator will be enough.');
+    process.exit(1);
+  }
+
+  // Verify if we're on node 10.x
+  const fs = require('fs');
+  if (!fs.promises) {
+    console.error('You\'re on an outdated Node.js version. Powercord requires you to run at least Node 10. Did you not run the .sh file?');
+    process.exit(1);
+  }
+
+  // Verify if deps have been installed. If not, install them automatically
+  const { dependencies } = require('../package.json');
+
+  try {
+    for (const dependency in dependencies) {
+      // noinspection JSUnfilteredForInLoop
+      require(dependency);
+    }
+  } catch (_) {
+    console.log('Some dependencies are not installed. Let\'s fix that...');
+    execSync('npm install --only=prod', {
+      cwd: resolve(__dirname, '..'),
+      stdio: [ null, null, null ]
+    });
+  }
+};
---
+++ injectors/linux.js
@@ -0,0 +1,45 @@
+/**
+ * Powercord, a lightweight @discordapp client mod focused on simplicity and performance
+ * Copyright (C) 2018-2020  aetheryx & Bowser65
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { join } = require('path');
+const { existsSync } = require('fs');
+const { execSync } = require('child_process');
+
+exports.getAppDir = async () => {
+  const discordProcess = execSync('ps x')
+    .toString()
+    .split('\n')
+    .map(s => s.split(' ').filter(Boolean))
+    .find(p => p[4] && (/discord?$/i).test(p[4]) && p.includes('--type=renderer'));
+
+  if (!discordProcess) {
+    console.log('Cannot find Discord process, falling back to legacy path detection.');
+    const paths = [
+      '/usr/share/discord',
+      '/usr/lib64/discord',
+      '/opt/discord',
+      '/opt/Discord'
+    ];
+    const discordPath = paths.find(path => existsSync(path));
+    return join(discordPath, 'resources', 'app');
+  }
+
+  const discordPath = discordProcess[4].split('/');
+  discordPath.splice(discordPath.length - 1, 1);
+  return join('/', ...discordPath, 'resources', 'app');
+};
