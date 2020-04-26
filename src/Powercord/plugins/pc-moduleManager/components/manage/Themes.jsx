b7a20137d1b825ff609f7d0d1664270222a0a65f
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2020-04-26 15:03:00 -0400

    New Release

--- src/Powercord/plugins/pc-moduleManager/components/manage/Themes.jsx
+++ src/Powercord/plugins/pc-moduleManager/components/manage/Themes.jsx
@@ -1,16 +1,19 @@
-const { React, getModule } = require('powercord/webpack');
+const { React, getModule, i18n: { Messages } } = require('powercord/webpack');
 const { TabBar } = require('powercord/components');
-const CodeMirror = require('./CodeMirror');
+const QuickCSS = require('./QuickCSS');
 const Base = require('./Base');
 
 class Themes extends Base {
   constructor () {
     super();
     this.state.tab = 'INSTALLED';
+    this.ConnectedQuickCSS = powercord.pluginManager.get('pc-moduleManager').settings.connectStore(QuickCSS);
   }
 
   render () {
     const { topPill, item } = getModule([ 'topPill' ], false);
+    const { ConnectedQuickCSS } = this;
+
     return (
       <>
         <div className='powercord-entities-manage-tabs'>
@@ -19,23 +22,27 @@ class Themes extends Base {
             onItemSelect={tab => this.setState({ tab })}
             type={topPill}
           >
-            <TabBar.Item className={item} selectedItem={this.state.tab} id='INSTALLED'>Manage</TabBar.Item>
-            <TabBar.Item className={item} selectedItem={this.state.tab} id='QUICK_CSS'>Quick CSS</TabBar.Item>
+            <TabBar.Item className={item} selectedItem={this.state.tab} id='INSTALLED'>
+              {Messages.MANAGE_USER_SHORTHAND}
+            </TabBar.Item>
+            <TabBar.Item className={item} selectedItem={this.state.tab} id='QUICK_CSS'>
+              {Messages.POWERCORD_QUICKCSS}
+            </TabBar.Item>
           </TabBar>
         </div>
-        {this.state.tab === 'INSTALLED' ? super.render() : this.renderQuickCSS()}
+        {this.state.tab === 'INSTALLED' ? super.render() : <ConnectedQuickCSS popoutComponent={ConnectedQuickCSS}/>}
       </>
     );
   }
 
-  renderQuickCSS () {
-    return (
-      <div className='powercord-quickcss'>
-        <div className='powercord-quickcss-header'></div>
-        <CodeMirror onReady={this.setupCodeMirror.bind(this)}/>
-        <div className='powercord-quickcss-footer'></div>
+  renderBody () {
+    return <div className='powercord-plugin-soon powercord-text'>
+      <div className='wumpus'>
+        <img src='/assets/8c998f8fb62016fcfb4901e424ff378b.svg' alt='wumpus'/>
       </div>
-    );
+      <p>{Messages.POWERCORD_THEMES_WIP1}</p>
+      <p>{Messages.POWERCORD_THEMES_WIP2}</p>
+    </div>;
   }
 
   // eslint-disable-next-line no-unused-vars
@@ -44,12 +51,13 @@ class Themes extends Base {
     return 'mhm';
   }
 
-  getItems () {
-    return this._sortItems([ ...powercord.styleManager.themes.values() ].filter(t => t.isTheme));
+  fetchMissing () { // @todo: better impl + i18n
+    // noinspection JSIgnoredPromiseFromCall
+    powercord.pluginManager.get('pc-moduleManager')._fetchEntities('themes');
   }
 
-  setupCodeMirror (cm) {
-    console.log(cm);
+  getItems () {
+    return this._sortItems([ ...powercord.styleManager.themes.values() ].filter(t => t.isTheme));
   }
 }
 
