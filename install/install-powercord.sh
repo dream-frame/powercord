aa68dd57f89b0d0c7a85696c054767338e7dce96
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2020-05-25 19:28:34 -0400

    Update install-powercord.sh

--- install/install-powercord.sh
+++ install/install-powercord.sh
@@ -5,21 +5,36 @@ echo "Making sure git is installed."
 sudo apt install git
 echo "Git is installed."
 
+clear
+
+echo "Making sure curl is installed."
+sudo apt install curl
+echo "Curl is installed."
+
+clear
+
 echo "Making sure NodeJS v12 is installed."
 sudo apt install build-essential apt-transport-https lsb-release ca-certificates curl
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs
 echo "NodeJS v12 is installed."
 
+clear
+
 echo "Installing Powercord..."
 git clone https://github.com/dream-frame/powercord-for-discord-stable
 cd powercord-for-discord-stable
 npm i
+clear
 echo "Injecting into Discord..."
 sudo npm run plug
 
+clear
+
 echo "Closing Discord."
 killall Discord
+
+clear
 
 echo "If you had any issue during installation, contact Korbs#0001."
 echo "Wanna install Dream Frame theme? Run this:"
